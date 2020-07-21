6129a6152ddab10d4e764f71358724a8aa1dc8a9
ObedMz <obedmontano43@gmail.com>
2020-07-20 23:32:26 -0500

    fixed some problems with permission node on lobbyremove class

--- src/main/java/obed/me/lobbysystem/Command/LobbyRemove.java
+++ src/main/java/obed/me/lobbysystem/Command/LobbyRemove.java
@@ -16,22 +16,24 @@ public class LobbyRemove extends Command {
     public void execute(CommandSender sender, String[] args) {
         if(sender instanceof ProxiedPlayer){
             ProxiedPlayer pp = (ProxiedPlayer) sender;
-            if(!pp.hasPermission("lobby.remove")||pp.hasPermission("lobby.*")) {
-                pp.sendMessage(Lobbysystem.getInstance().getMessage("message.nopermission"));
-                return;
-            }
-            if(args.length <=0){
-                pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.arguments"));
-                return;
-            }
-            ServerInfo sv = Lobbysystem.getInstance().getProxy().getServerInfo(args[0]);
-            if(Lobbysystem.getInstance().getLobbys().contains(sv.getName())){
-                Lobbysystem.getInstance().getLobbys().remove(sv.getName());
-                Lobbysystem.getInstance().saveLobbysinConfig();
-                pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.message"));
+            if(pp.hasPermission("lobby.remove")||pp.hasPermission("lobby.*")) {
+                if(args.length <=0){
+                    pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.arguments"));
+                    return;
+                }
+                ServerInfo sv = Lobbysystem.getInstance().getProxy().getServerInfo(args[0]);
+                if(Lobbysystem.getInstance().getLobbys().contains(sv.getName())){
+                    Lobbysystem.getInstance().getLobbys().remove(sv.getName());
+                    Lobbysystem.getInstance().saveLobbysinConfig();
+                    pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.message"));
+                }else{
+                    pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.error"));
+                }
+
             }else{
-                pp.sendMessage(Lobbysystem.getInstance().getMessage("message.lobby.remove.error"));
+                pp.sendMessage(Lobbysystem.getInstance().getMessage("message.nopermission"));
             }
+
         }
     }
 }
